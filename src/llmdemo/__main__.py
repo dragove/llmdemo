85526f1a51e1e379c3120062977cf86f5fed9b97
dragove <dragove@qq.com>
2023-06-13 23:26:16 +0800

    repl qa

--- src/llmdemo/__main__.py
+++ src/llmdemo/__main__.py
@@ -1,2 +1,11 @@
-from llmdemo import hello
-print(hello())
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "THUDM/chatglm-6b-int4", trust_remote_code=True)
+model = AutoModel.from_pretrained(
+    "THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
+model = model.eval()
+history = []
+while (q := input("Q: ")):
+    response, history = model.chat(tokenizer, q, history)
+    print(f"A: {response}\n")
